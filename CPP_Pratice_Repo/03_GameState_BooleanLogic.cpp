faeeeeb94b958fe0489e8ee59d33e04e3e56eb41
sairamyedida06 <sairamyedidaoffl@gmail.com>
2026-01-06 21:27:21 +0530

    Add game state boolean guard logic

---
+++ CPP_Pratice_Repo/03_GameState_BooleanLogic.cpp
@@ -0,0 +1,19 @@
+bool isGameOver;
+bool isPaused;
+int health;
+
+// part A
+
+if (!isGameOver && !isPaused && health > 0)
+{
+	
+}
+
+// part B
+
+if (health <= 0 && !isGameOver)
+{
+	isGameOver = true;
+}
+
+
